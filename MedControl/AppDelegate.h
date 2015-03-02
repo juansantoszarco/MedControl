a9fc1811e8eea6a1599a09f3f5ecda3f5e0a37fe
Juan Antonio de los Santos Zarco <JuanAntonio@iMac-de-Juan.local>
2015-03-02 10:21:49 +0100

    Initial Commit

---
+++ MedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MedControl
+//
+//  Created by Juan Antonio de los Santos Zarco on 2/3/15.
+//  Copyright (c) 2015 Juan Antonio de los Santos Zarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MedControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MedControl
+//
+//  Created by Juan Antonio de los Santos Zarco on 2/3/15.
+//  Copyright (c) 2015 Juan Antonio de los Santos Zarco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
